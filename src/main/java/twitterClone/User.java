369a8f0f803b37b00cd81bea09e7a68b865e1b27
Romano <n0252325@lm.lmig.com>
2017-06-14 18:25:31 -0400

    username/handle checks

--- src/main/java/twitterClone/User.java
+++ src/main/java/twitterClone/User.java
@@ -35,14 +35,60 @@ public class User {
 					
 	        try (Connection conn = insertConnect();
 	                PreparedStatement pstmt = conn.prepareStatement(sql)) {
-	            pstmt.setString(1, username);
-	            pstmt.setString(2, handle);
-	            pstmt.setString(3, display_name);
-	            pstmt.setString(4, password1);
-	            pstmt.executeUpdate();
+	            if (checkUser(username) == "pass" && checkHandle(handle) == "pass") {
+		        	pstmt.setString(1, username);
+		            pstmt.setString(2, handle);
+		            pstmt.setString(3, display_name);
+		            pstmt.setString(4, password1);
+		            pstmt.executeUpdate(); }
+	            else if (checkUser(username) == "pass" && checkHandle(handle) == "dupHandle"){
+	            	System.out.println("That handle is already in use. Please choose another.");
+		        } else if (checkUser(username) == "dupUsername" && checkHandle(handle) == "pass") {
+		        	System.out.println("That username is already in use. Please choose another.");
+		        } else {
+		        	System.out.println("The username and handle you've selected are already in use.");
+		        }
+	            
 	        } catch (SQLException e) {
 	            System.out.println(e.getMessage());
 	        }
 		}
+	}
+	
+	
+	public String checkUser(String username) {
+		String sqlUsername = "SELECT count(*) FROM User where user_name = \"" + username + "\"";
+        
+        try (Connection conn = insertConnect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sqlUsername)){
+            
+            if (rs.getInt("count(*)") >= 1) {
+            	return "dupUsername";
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return "pass";
+	}
+	
+	public String checkHandle(String handle) {
+		String sqlHandle = "SELECT count(*) FROM User where handle = \"" + handle + "\"";
+		System.out.println(sqlHandle);
+		
+        try (Connection conn = insertConnect();
+                Statement stmt  = conn.createStatement();
+                ResultSet rs    = stmt.executeQuery(sqlHandle)){
+               
+               if (rs.getInt("count(*)") >= 1) {
+               	return "dupHandle";
+               }
+               
+           } catch (SQLException e) {
+               System.out.println(e.getMessage());
+           }
+           return "pass";
+   	
 	}
 }
