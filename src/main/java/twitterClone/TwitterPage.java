77af25bdc1d1b4772b0fe84e28b6df5d77a5c3f3
Romano <n0252325@lm.lmig.com>
2017-06-15 12:46:52 -0400

    new updates

--- src/main/java/twitterClone/TwitterPage.java
+++ src/main/java/twitterClone/TwitterPage.java
@@ -41,11 +41,12 @@ public class TwitterPage {
 //          System.out.print("entering test2");
           String username = req.queryParams("username");
           String psw1 = req.queryParams("password1");
-//          User user = new User();
-          System.out.println("Username entered = "+username);
-          System.out.println("Password entered = "+psw1);
-          System.out.println("Call checkUser and navigate to user Home Page if valid.");
-          return "exiting test2";
+          User user = new User();
+          String returnMessage = user.checkLogin(username, psw1);
+//          System.out.println("Username entered = "+username);
+//          System.out.println("Password entered = "+psw1);
+//          System.out.println("Call checkUser and navigate to user Home Page if valid.");
+          return returnMessage;
       });
 		
         post("/test", (req, res) -> {
@@ -58,7 +59,7 @@ public class TwitterPage {
             User user = new User();
 //            int returnCde = 0;
 //            returnCde=user.insertUser(username, handle, display_name, psw1, psw2);
-            user.insertUser(username, handle, display_name, psw1, psw2);
+            String returnMessage = user.insertUser(username, handle, display_name, psw1, psw2);
 //            if(returnCde == -1){
 //            	System.out.println("Passwords entered are not the same.");
 //            	System.out.println("show register page again");
@@ -76,7 +77,7 @@ public class TwitterPage {
 //                System.out.println("bad input");
 //            }
 //            return "failure";
-            return "in test";
+            return returnMessage;
         });
 		
 		get("/timeLine", (req, res) -> {
--- src/main/java/twitterClone/User.java
+++ src/main/java/twitterClone/User.java
@@ -27,31 +27,40 @@ public class User {
 	}
 
 	
-	public void insertUser(String username,String handle,String display_name,String password1,String password2){
+	public String insertUser(String username,String handle,String display_name,String password1,String password2){
 		String sql = "INSERT INTO User(user_name,handle,display_name,password) VALUES (?,?,?,?)";
+		String returnMessage = "";
 		if (!password1.equals(password2)){
 //			System.out.println("Passwords entered are not the same.");
+			returnMessage = "Passwords entered are not the same.";
+			return returnMessage;
 		} else{
-					
 	        try (Connection conn = insertConnect();
 	                PreparedStatement pstmt = conn.prepareStatement(sql)) {
-	            if (checkUser(username) == "pass" && checkHandle(handle) == "pass") {
+	        	if (checkHandle(handle) == "incorrectHandleTag") {
+		        	returnMessage = "Please make sure your handle begins with '@'.";
+		        } else if (checkUser(username) == "pass" && checkHandle(handle) == "pass") {
 		        	pstmt.setString(1, username);
 		            pstmt.setString(2, handle);
 		            pstmt.setString(3, display_name);
 		            pstmt.setString(4, password1);
-		            pstmt.executeUpdate(); }
+		            pstmt.executeUpdate();
+		            returnMessage = "";}
 	            else if (checkUser(username) == "pass" && checkHandle(handle) == "dupHandle"){
 	            	System.out.println("That handle is already in use. Please choose another.");
+	            	returnMessage = "That handle is already in use. Please choose another.";
 		        } else if (checkUser(username) == "dupUsername" && checkHandle(handle) == "pass") {
 		        	System.out.println("That username is already in use. Please choose another.");
+		        	returnMessage = "That username is already in use. Please choose another.";
 		        } else {
 		        	System.out.println("The username and handle you've selected are already in use.");
+		        	returnMessage = "The username and handle you've selected are already in use.";
 		        }
 	            
 	        } catch (SQLException e) {
 	            System.out.println(e.getMessage());
 	        }
+	        return returnMessage;
 		}
 	}
 	
@@ -61,7 +70,7 @@ public class User {
         
         try (Connection conn = insertConnect();
              Statement stmt  = conn.createStatement();
-             ResultSet rs    = stmt.executeQuery(sqlUsername)){
+             ResultSet rs    = stmt.executeQuery(sqlUsername)) {
             
             if (rs.getInt("count(*)") >= 1) {
             	return "dupUsername";
@@ -79,16 +88,52 @@ public class User {
 		
         try (Connection conn = insertConnect();
                 Statement stmt  = conn.createStatement();
-                ResultSet rs    = stmt.executeQuery(sqlHandle)){
+                ResultSet rs    = stmt.executeQuery(sqlHandle)) {
                
-               if (rs.getInt("count(*)") >= 1) {
+        	if (!handle.startsWith("@")) {
+         	   return "incorrectHandleTag";
+            } else if (rs.getInt("count(*)") >= 1) {
                	return "dupHandle";
                }
                
            } catch (SQLException e) {
                System.out.println(e.getMessage());
            }
+	
            return "pass";
-   	
 	}
+	
+	
+	
+	public String checkLogin(String username, String password) {
+		String sqlUsername = "SELECT count(*) FROM User where user_name = \"" + username + "\"";
+		String sqlUsernamePassword = "SELECT count(*) FROM User where user_name = \"" + username + "\"" + "AND password = \"" +  password + "\"";
+		String returnMessage = "";
+		
+        try (Connection conn = insertConnect();
+                Statement stmt  = conn.createStatement();
+                ResultSet rs    = stmt.executeQuery(sqlUsername);
+        		ResultSet rs1   = stmt.executeQuery(sqlUsernamePassword)) {
+        	System.out.println(rs);
+        	System.out.println(rs1);
+            
+        	if (rs.getInt("count(*)") == 0) { //if username doesn't exist in database
+            	System.out.println(rs.getInt("count(*)"));
+        		returnMessage = "The username you entered doesn't exist.";
+//            	return returnMessage;
+            } else if (rs1.getInt("count(*)") == 0) { //if username + password combo doesn't exist in database
+            	System.out.println(rs1.getInt("count(*)"));
+        		returnMessage = "The password you've entered is incorrect."; }
+//            	return returnMessage; }
+             else {
+            	returnMessage = "";
+            }
+               
+           } catch (SQLException e) {
+               System.out.println(e.getMessage());
+           }
+	
+           return returnMessage;
+	}
+	
 }
